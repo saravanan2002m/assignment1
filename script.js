6e64a48aa19b6ad9bdc036709c0213fc8e097588
Saravanan <155075903+saravanan2002m@users.noreply.github.com>
2024-07-02 21:03:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let para=document.getElementById("para");
+let button=document.getElementById("btn");
+button.addEventListener('click',()=>{
+    para.textContent="Assisgnment done. Thankyou..."
+})
